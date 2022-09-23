be8be0744c6949658d58833025dca0c75527d9eb
shn <topdog.shn.01@gmail.com>
2022-09-23 23:48:05 +0900

    ✨ Footerコンポーネント作成

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
@@ -5,12 +6,10 @@ export default function Home() {
 	return (
 		<>
 			<Header />
-
 			<main>
 				<Hero />
 			</main>
-
-			<footer>FOOTER</footer>
+			<Footer />
 		</>
 	);
 }
