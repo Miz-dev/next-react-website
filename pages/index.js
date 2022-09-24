c74ea414e09be9b2c95b496a5b9fc9e4545c488c
shn <topdog.shn.01@gmail.com>
2022-09-24 21:15:57 +0900

    ✨ Hero画像をpropsでONにする

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Hero from "components/Hero";
 
 export default function Home() {
-	return <Hero title="CUBE" subtitle="アウトプットしていくサイトです" />;
+	return (
+		<Hero title="CUBE" subtitle="アウトプットしていくサイトです" imageOn />
+	);
 }
