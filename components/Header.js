5b346203e0f8c7a1552479e727ac39157c87fe31
shn <topdog.shn.01@gmail.com>
2022-10-01 11:28:46 +0900

    💄 headerの横幅調整

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,17 @@
 import Logo from "./Logo";
 import Nav from "./Nav";
 import styles from "../styles/header.module.css";
+import Container from "./container";
 
 export default function Header() {
 	return (
 		<header>
-			<div className={styles.flexContainer}>
-				<Logo boxOn />
-				<Nav />
-			</div>
+			<Container large>
+				<div className={styles.flexContainer}>
+					<Logo boxOn />
+					<Nav />
+				</div>
+			</Container>
 		</header>
 	);
 }
