7e23d9a172fd236058857eae5f87b3922816914d
shn <topdog.shn.01@gmail.com>
2022-10-28 22:05:45 +0900

    ✨ 記事ページにメタデータ追加

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -10,6 +10,8 @@ import {
 import Image from "next/image";
 import ConvertBody from "components/ConvertBody";
 import PostCategories from "components/PostCategories";
+import { extractText } from "lib/extractText";
+import Meta from "components/Meta";
 
 export default function Schedule({
 	title,
@@ -17,9 +19,17 @@ export default function Schedule({
 	content,
 	eyecatch,
 	categories,
+	description,
 }) {
 	return (
 		<Container>
+			<Meta
+				pageTitle={title}
+				pageDesc={description}
+				pageImg={eyecatch.url}
+				pageImgW={eyecatch.width}
+				pageImgH={eyecatch.height}
+			/>
 			<article>
 				<PostHeader title={title} subtitle="Blog Article" publish={publish} />
 
@@ -55,6 +65,8 @@ export async function getStaticProps() {
 
 	const post = await getPostBySlug(slug);
 
+	const description = extractText(post.content);
+
 	return {
 		props: {
 			title: post.title,
@@ -62,6 +74,7 @@ export async function getStaticProps() {
 			content: post.content,
 			eyecatch: post.eyecatch,
 			categories: post.categories,
+			description: description,
 		},
 	};
 }
