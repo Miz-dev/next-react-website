395db3f44a44547a1162d29fa2b2b65ff00385fe
shn <topdog.shn.01@gmail.com>
2022-11-19 21:46:17 +0900

    ✨ 記事一覧にアイキャッチ画像追加

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,8 @@ import Hero from "components/Hero";
 import Meta from "components/meta";
 import Posts from "components/posts";
 import { getAllPosts } from "lib/api";
+import { getPlaiceholder } from "plaiceholder";
+import { eyecatchLocal } from "lib/constants";
 
 export default function Blog({ posts }) {
 	return (
@@ -18,6 +20,14 @@ export default function Blog({ posts }) {
 
 export async function getStaticProps() {
 	const posts = await getAllPosts();
+
+	for (const post of posts) {
+		if (!post.hasOwnProperty("eyecatch")) {
+			post.eyecatch = eyecatchLocal;
+		}
+		const { base64 } = await getPlaiceholder(post.eyecatch.url);
+		post.eyecatch.blurDataURL = base64;
+	}
 
 	return {
 		props: {
