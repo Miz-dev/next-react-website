876726420e45f630035ad4119fce793f87f7136e
shn <topdog.shn.01@gmail.com>
2022-11-19 21:29:22 +0900

    ✨ 記事一覧コンポーネント作成

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,12 +1,27 @@
 import Container from "components/container";
 import Hero from "components/Hero";
 import Meta from "components/meta";
+import Posts from "components/posts";
+import { getAllPosts } from "lib/api";
 
-export default function Blog() {
+export default function Blog({ posts }) {
 	return (
 		<Container>
 			<Meta pageTitle="ブログ" pageDesc="ブログの記事一覧" />
+
 			<Hero title="Blog" subtitle="Recent Posts" />
+
+			<Posts posts={posts} />
 		</Container>
 	);
+}
+
+export async function getStaticProps() {
+	const posts = await getAllPosts();
+
+	return {
+		props: {
+			posts: posts,
+		},
+	};
 }
